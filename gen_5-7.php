b1e09d628f33e75eb9ff8756757ee3f512eaacd7
novaskyland <nick.heitsch@gmail.com>
2016-11-21 22:29:26 -0500

    Content Layout Adjustment
    Generation pages edited so text content is in one column to the left,
    and all other content will be on the right (can add videos and more)

--- gen_5-7.php
+++ gen_5-7.php
@@ -6,31 +6,36 @@
 			
 			<h2>Generations V-VII</h2>
 
-			<h3>Generation V (2010-2013)</h3>
-			
-			<img class="img-responsive" src="images/5th.png" alt="5thGen">
-			
-			<p>The fifth generation of Pokémon began on September 18, 2010 with the release of Pokémon Black and White in Japan. They were then released in North America, Europe, and Australia in March 2011. They were released on the Nintendo DS, the same console as its predecessing generation.</p>
+			<div class="gen-block">
+				<h3>Generation V (2010-2013)</h3>
+
+				<div class="gen-content">			
+					<p>The fifth generation of Pokémon began on September 18, 2010 with the release of Pokémon Black and White in Japan. They were then released in North America, Europe, and Australia in March 2011. They were released on the Nintendo DS, the same console as its predecessing generation.</p>
+					<p>The games take place in the Unova region. New features include the C-Gear, a feature where players can use Wi-Fi options and customizations; two new battle methods ("Triple Battles", where three Pokémon are sent out at once, and Rotation Battles, where three Pokémon are also sent out at the same time, but the trainer can switch one Pokémon out of the three that are present); "Battle Tests", where trainers battle each other to see who has stronger Pokémon; the Pokémon Musicals (similar to Pokémon Contests), which have trainers use their Pokémon to dance in a theater with other Pokémon; and the ability to never waste Technical Machines (TMs), even when found the first time.</p>
+					<p>This generation introduced a total of 156 new Pokémon (beginning with Victini and ending with Genesect), the most of any generation. It was also the first generation where the first new Pokémon in National Pokédex order (Victini) is not a starter. It also introduced another new feature, the seasons, from which two Pokémon (Deerling and Sawsbuck) represent them. Unlike previous generations, which would introduce some species of Pokémon that were evolutionary relatives of older-generation Pokémon, the fifth generation's selection was all-original, in an attempt to make the primary versions feel like a brand-new game.</p>
+					<p>The other core series games, and the additions to Black and White, titled Pokémon Black 2 and White 2, are direct sequels. They take place in the Unova region two years later, and were released in Japan on June 23, 2012 and in North America, Australia, and Europe in October of that year for Nintendo DS. They are somewhat different of their predecessors; there are different protagonist trainers, and many of the other important characters have changed as well. The games also introduced a new feature, the "Pokémon World Tournament", where trainers can battle gym leaders and champions from older regions, including Unova. The games also broke the tradition of releasing a third version as the addition to the primary versions.</p>
+					<p>Spin-off fifth generation games include sequels Pokémon Rumble Blast and Pokémon Mystery Dungeon: Gates to Infinity for Nintendo 3DS, PokéPark 2: Wonders Beyond for Wii, and Pokémon Rumble U for Wii U, a downloadable game. Others include Learn with Pokémon: Typing Adventure (a typing game) and Pokémon Conquest (a crossover game) for Nintendo DS, and downloadable reference applications Pokédex 3D, Pokédex 3D Pro (for Nintendo 3DS), and Pokédex for iOS (for iOS devices), which allows players to view information of Pokémon species while they have 3D models.</p>
+				</div>
+
+				<img class="img-responsive gen-img" src="images/5th.png" alt="5thGen">
+
+			</div>
+
+
+			<div class="gen-block">
+				<h3>Generation VI (2013-2016)</h3>
 				
-			<p>The games take place in the Unova region. New features include the C-Gear, a feature where players can use Wi-Fi options and customizations; two new battle methods ("Triple Battles", where three Pokémon are sent out at once, and Rotation Battles, where three Pokémon are also sent out at the same time, but the trainer can switch one Pokémon out of the three that are present); "Battle Tests", where trainers battle each other to see who has stronger Pokémon; the Pokémon Musicals (similar to Pokémon Contests), which have trainers use their Pokémon to dance in a theater with other Pokémon; and the ability to never waste Technical Machines (TMs), even when found the first time.</p>
+				<div class="gen-content">
+					<p>On December 24, 2012, Japanese magazine Nintendo Dream posted a greetings card sent out by Game Freak. In the card, Junichi Masuda exclaimed that during 2013, they intend to further evolve the world of Pokémon. On December 29, 2012, a new Pokémon Black 2 and White 2 commercial aired in Japan, and ending with a message, informing Pokémon fans that the latest news would be announced on January 8, 2013. On January 4, 2013, both the Japanese and English official Pokémon website confirmed that an announcement would be made on January 8. On January 7, 2013, the official Japanese website explained that the Nintendo president, Satoru Iwata would hold a 10-minute "Pokémon Direct" video conference to announce the big Pokémon news. On January 8, 2013, Satoru Iwata announced the sixth generation of Pokémon, with the new paired games, Pokémon X and Y, which were released on the Nintendo 3DS on October 12, 2013 worldwide.[2] The X and Y games are rendered in full 3D; however, only select parts of the game can be displayed in stereoscopic 3D. The video introduced the player characters, the starter Pokémon; Grass-type Chespin (Japanese: Harimaron (ハリマロン?)), the Fire-type Fennekin (Japanese: Fokko (フォッコ?)), and the Water-type Froakie (Japanese: Keromatsu (ケロマツ?)), and two other Pokémon, not named until later; a bird-like Pokémon called Yveltal (イベルタル Iberutaru?) having a shape similar to the letter Y and a deer-like Pokémon called Xerneas (ゼルネアス Zeruneasu?) with X-shapes in its eyes. A month later, Sylveon(Japanese: Nymphia (ニンフィア Ninfia?)), a new evolved form of Eevee belonging to the games' new Fairy Type was revealed.</p>
+					<p>This generation introduced a total of 72 new Pokémon, the new Fairy type, Mega Evolution, the Kalos region, Trainer customization, Super Training, and three new battle modes: Sky Battles, Horde Encounters, and Inverse Battle. This generation is also the first to be compatible with Pokémon Bank. Greninja, the final evolved form of Froakie, would later go on to represent the sixth generation of Pokémon in the hit fighting game, Super Smash Bros. for Nintendo 3DS and Wii U.</p>
+					<p>On May 7, 2014, Nintendo revealed the games Pokémon Omega Ruby and Alpha Sapphire in a teaser trailer, remakes of the third generation games Pokémon Ruby and Sapphire. They were released worldwide in November 2014. On August 26, 2014, Pokkén Tournament was announced and was released on July 16, 2015 in Japanese arcades and was released on March 18, 2016 worldwide for Wii U. It was developed by Bandai Namco Entertainment.</p>
+				</div>
+
 				
-			<p>This generation introduced a total of 156 new Pokémon (beginning with Victini and ending with Genesect), the most of any generation. It was also the first generation where the first new Pokémon in National Pokédex order (Victini) is not a starter. It also introduced another new feature, the seasons, from which two Pokémon (Deerling and Sawsbuck) represent them. Unlike previous generations, which would introduce some species of Pokémon that were evolutionary relatives of older-generation Pokémon, the fifth generation's selection was all-original, in an attempt to make the primary versions feel like a brand-new game.</p>
-				
-			<p>The other core series games, and the additions to Black and White, titled Pokémon Black 2 and White 2, are direct sequels. They take place in the Unova region two years later, and were released in Japan on June 23, 2012 and in North America, Australia, and Europe in October of that year for Nintendo DS. They are somewhat different of their predecessors; there are different protagonist trainers, and many of the other important characters have changed as well. The games also introduced a new feature, the "Pokémon World Tournament", where trainers can battle gym leaders and champions from older regions, including Unova. The games also broke the tradition of releasing a third version as the addition to the primary versions.</p>
-				
-			<p>Spin-off fifth generation games include sequels Pokémon Rumble Blast and Pokémon Mystery Dungeon: Gates to Infinity for Nintendo 3DS, PokéPark 2: Wonders Beyond for Wii, and Pokémon Rumble U for Wii U, a downloadable game. Others include Learn with Pokémon: Typing Adventure (a typing game) and Pokémon Conquest (a crossover game) for Nintendo DS, and downloadable reference applications Pokédex 3D, Pokédex 3D Pro (for Nintendo 3DS), and Pokédex for iOS (for iOS devices), which allows players to view information of Pokémon species while they have 3D models.</p>
-				
-				
-			<h3>Generation VI (2013-2016)</h3>
-			
-			<img class="img-responsive" src="images/6th.png" alt="6thGen">
-			
-			<p>On December 24, 2012, Japanese magazine Nintendo Dream posted a greetings card sent out by Game Freak. In the card, Junichi Masuda exclaimed that during 2013, they intend to further evolve the world of Pokémon. On December 29, 2012, a new Pokémon Black 2 and White 2 commercial aired in Japan, and ending with a message, informing Pokémon fans that the latest news would be announced on January 8, 2013. On January 4, 2013, both the Japanese and English official Pokémon website confirmed that an announcement would be made on January 8. On January 7, 2013, the official Japanese website explained that the Nintendo president, Satoru Iwata would hold a 10-minute "Pokémon Direct" video conference to announce the big Pokémon news. On January 8, 2013, Satoru Iwata announced the sixth generation of Pokémon, with the new paired games, Pokémon X and Y, which were released on the Nintendo 3DS on October 12, 2013 worldwide.[2] The X and Y games are rendered in full 3D; however, only select parts of the game can be displayed in stereoscopic 3D. The video introduced the player characters, the starter Pokémon; Grass-type Chespin (Japanese: Harimaron (ハリマロン?)), the Fire-type Fennekin (Japanese: Fokko (フォッコ?)), and the Water-type Froakie (Japanese: Keromatsu (ケロマツ?)), and two other Pokémon, not named until later; a bird-like Pokémon called Yveltal (イベルタル Iberutaru?) having a shape similar to the letter Y and a deer-like Pokémon called Xerneas (ゼルネアス Zeruneasu?) with X-shapes in its eyes. A month later, Sylveon(Japanese: Nymphia (ニンフィア Ninfia?)), a new evolved form of Eevee belonging to the games' new Fairy Type was revealed.</p>
-				
-			<p>This generation introduced a total of 72 new Pokémon, the new Fairy type, Mega Evolution, the Kalos region, Trainer customization, Super Training, and three new battle modes: Sky Battles, Horde Encounters, and Inverse Battle. This generation is also the first to be compatible with Pokémon Bank. Greninja, the final evolved form of Froakie, would later go on to represent the sixth generation of Pokémon in the hit fighting game, Super Smash Bros. for Nintendo 3DS and Wii U.</p>
-				
-			<p>On May 7, 2014, Nintendo revealed the games Pokémon Omega Ruby and Alpha Sapphire in a teaser trailer, remakes of the third generation games Pokémon Ruby and Sapphire. They were released worldwide in November 2014. On August 26, 2014, Pokkén Tournament was announced and was released on July 16, 2015 in Japanese arcades and was released on March 18, 2016 worldwide for Wii U. It was developed by Bandai Namco Entertainment.</p>
-				
+				<img class="img-responsive gen-img" src="images/6th.png" alt="6thGen">
+			</div>
+
+
 			<h3>Generation VII (2016-Present)</h3>
 
 			<img class="img-responsive" src="images/7th.png" alt="7thGen">
@@ -38,7 +43,8 @@
 			<p>During a Nintendo Direct presentation on February 26, 2016, two new Pokémon titles were announced: Pokémon Sun and Moon. The first installments in the seventh generation of Pokémon games, they are set to be released on 18 November 2016. The games will be compatible with other Nintendo 3DS titles, including Pokémon X and Y; Pokémon Omega Ruby and Alpha Sapphire; and the Virtual Console re-releases of Pokémon Red, Blue, and Yellow.</p>
 
 			<p>In July 2016, Niantic & Nintendo released a free-to-play augmented-reality game titled Pokémon Go which was released on Android & iOS devices. Upon its release, the game has become a global phenomenon. Unfortunately, the game has also received a lot of negative criticism.</p>
-			
+		
+			</div>
 		</article>
 
 		<?php include "inc/footer.inc";?>
