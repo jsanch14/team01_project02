17c0dbdaead0d15ae6c0903adb748b05f25c5abb
jsanch14 <jorgesanchezdellano@Jorges-MacBook-Pro.local>
2016-11-21 23:02:08 -0500

    slider buttons

--- index.php
+++ index.php
@@ -2,12 +2,45 @@
 		
 		<?php include "inc/nav.inc";?>
 		
-		<div class="slider">
-			<img class="img-responsive" src="images/poke1.png" alt="poke1">
-			<img class="img-responsive" src="images/poke2.png" alt="poke2">
-			<img class="img-responsive" src="images/poke3.png" alt="poke3">
-		</div>
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/poke1.png" alt="poke">
+   
+    </div>
+
+    <div class="item">
+      <img src="images/poke2.png" alt="poke">
+     
+    </div>
+
+    <div class="item">
+      <img src="images/poke3.png" alt="poke">
+      
+
+    
+  </div>
+</div>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="container">
 		<article class="col-md-8">
 
 			<h2>Overview</h2>
